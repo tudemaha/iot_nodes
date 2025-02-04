2697b0f675e3d2f35813973cd13f431341f5df97
tudemaha <gede_mahardika88@yahoo.co.id>
2025-02-04 14:31:24 +0800

    feat: add camera capture
    move sensor function to sensor.ino

--- nodes.ino
+++ nodes.ino
@@ -1,6 +1,7 @@
 #include "DHT.h"
 #include "MQ135.h"
 #include "TinyGPS.h"
+#include "esp_camera.h"
 #include "helper.h"
 
 #define DHTPIN 2
@@ -32,83 +33,14 @@ void setup() {
 
   Serial.println("Starting pH Sensor...");
   pinMode(PH_PIN, INPUT);
+
+  Serial.println("Starting Camera...");
+  initCamera();
 }
 
 void loop() {
-  float ph = readPH();
-  Serial.println(ph);
-  Serial.println();
-  delay(1000);
+  float test = readPH();
 
-}
+  delay(3000);
 
-dhtReading readDht() {
-  dhtReading reading;
-
-  reading.temperature = dht.readTemperature();
-  reading.humidity = dht.readHumidity();
-
-  if(isnan(reading.temperature) || isnan(reading.humidity)) {
-    reading.temperature = reading.humidity = 0;
-    Serial.println("Failed to read from DHT sensor.");
-  }
-
-  return reading;
-}
-
-float readGas(float temperature, float humidity) {
-  float ppm = mq135.getCorrectedPPM(temperature, humidity);
-
-  return ppm;
-}
-
-float readSoilMoisture() {
-  int sensor = analogRead(SOILPIN);
-  float soilMoisture = sensor * (100.0 / 4095.0);
-
-  return soilMoisture;
-}
-
-String readGPS() {
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
-
-  for(unsigned long start = millis(); millis() - start < 1000;) {
-    while(Serial1.available()) {
-      char c = Serial1.read();
-      if(gps.encode(c)) newData = true;
-    }
-  }
-
-  if(newData) {
-    float flat, flon;
-    unsigned long age;
-
-    gps.f_get_position(&flat, &flon, &age);
-    String latLon = String(flat, 6) + ", " + String(flon, 6);
-
-    return latLon;
-  }
-
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print("CHARS=");
-  Serial.print(chars);
-  Serial.print(" SENTENCES=");
-  Serial.print(sentences);
-  Serial.print(" CSUM ERR=");
-  Serial.println(failed);
-  
-  if(chars == 0)
-    Serial.println("No charactes received, check wiring!");
-  
-  return "0.0, 0.0";
-}
-
-float readPH() {
-  int phADC = analogRead(PH_PIN);
-  Serial.println(phADC);
-  float ph = -0.01734 * phADC + 7.38552;
-  
-  return ph;
 }
