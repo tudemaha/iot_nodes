57842f72bdc669c874c9d17c27b9b77179003cc0
tudemaha <gede_mahardika88@yahoo.co.id>
2025-02-09 09:12:31 +0800

    finish save picture

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,14 +1,18 @@
 #include "esp_camera.h"
 
 #define NANO_RX 3
-#define NNAO_TX 1
+#define NANO_TX 1
 
 void setup() {
   Serial2.begin(9600, SERIAL_8N1, NANO_RX, NANO_TX);
 
+  initCamera();
+  initSD();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  camera_fb_t* image = takePicture();
+  saveImage(image);
+  delay(100);
+  esp_camera_fb_return(image);
 }
